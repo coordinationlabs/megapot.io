40a39b1b6f3614c2a21ea77f6537160d066ab936
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2025-09-29 15:59:49 -0600

    Routes

---
+++ src/app/liquidity/page.tsx
@@ -0,0 +1,7 @@
+export default function LiquidityPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-gray-900">Liquidity</h1>
+    </div>
+  );
+}
