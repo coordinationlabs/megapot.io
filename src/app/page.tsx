bceba8680473d864b1e4b649c36ac15b124cb474
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2025-09-29 15:12:36 -0600

    Font setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-svh flex items-center justify-center">
+    <main className="min-h-svh flex items-center justify-center text-primary">
       <h1 className="text-2xl font-semibold">Megapot V2</h1>
     </main>
   );
