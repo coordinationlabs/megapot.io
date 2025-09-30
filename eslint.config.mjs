5e5b0b4d4f10392a1be8be33f1806710d4b24031
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2025-09-30 13:50:43 -0600

    Updated ecma version to 2022

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
+    languageOptions: {
+      ecmaVersion: 2022,
+    },
     plugins: {
       prettier: prettierPlugin,
     },
